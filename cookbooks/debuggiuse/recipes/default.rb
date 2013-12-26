acf09c99f632b62d7995ede735bb5bd5e6976c9d
Giuseppe Rota <rota.giuseppe@gmail.com>
2013-12-26 16:13:37 +0000

    debug recipe

--- cookbooks/debuggiuse/recipes/default.rb
+++ cookbooks/debuggiuse/recipes/default.rb
@@ -1,8 +1,16 @@
-#
-# Cookbook Name:: debuggiuse
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+# this stuff does not work, not sure if this is because a change in the scope of vars in blocks in ruby post 1.8.7
+
+#local_var = ''
+ruby_block 'testing' do
+  block do
+    local_var = 'ciao'
+    puts local_var
+  end
+  action :create
+end
+#puts 'dopo' + local_var
+
+template '/tmp/sitcazzi' do
+  source 'sticazzi.erb'
+  variables(:primavar => local_var)
+end
