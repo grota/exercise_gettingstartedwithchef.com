7bf20baef3faa5afe583f617b6b135a26ce74c43
Giuseppe Rota <rota.giuseppe@gmail.com>
2013-12-26 18:53:37 +0000

    move wordpress stuff to a definition

---
+++ cookbooks/phpapp/definitions/wordpress_site.rb
@@ -0,0 +1,64 @@
+define :wordpress_site, :path => "/var/www/phpapp", :database => "phpapp", :db_username => "phpapp", :db_password => "phpapp", :template => "site.conf.erb" do
+
+# download and extract wordpress
+wordpress_latest = Chef::Config[:file_cache_path] + "/wordpress-latest.tar.gz"
+
+remote_file wordpress_latest do
+  source "http://wordpress.org/latest.tar.gz"
+  mode "0644"
+  checksum 'c419de49816b6483ab387567222898c02e8acd6ce64d6466a38f2fc05ebefb85'
+end
+
+directory params[:path] do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  recursive true
+end
+
+execute "untar-wordpress" do
+  cwd params[:path]
+  command "tar --strip-components 1 -xzf " + wordpress_latest
+  creates params[:path] + "/wp-settings.php"
+end
+
+#install wp
+wp_secrets = Chef::Config[:file_cache_path] + '/wp-secrets.php'
+
+remote_file wp_secrets do
+  source 'https://api.wordpress.org/secret-key/1.1/salt/'
+  action :create_if_missing
+  mode 0644
+end
+
+ruby_block 'fetch-salt-data' do
+  block do
+    salt_data = File.read(wp_secrets)
+  end
+  action :create
+end
+salt_data = File.read(wp_secrets)
+
+template params[:path] + '/wp-config.php' do
+  source 'wp-config.php.erb'
+  mode 0755
+  owner 'root'
+  group 'root'
+  variables(
+    :database        => params[:database],
+    :user            => params[:db_username],
+    :password        => params[:db_password],
+    :wp_secrets      => salt_data)
+end
+
+# TODO
+docroot = params[:path]
+server_name = params[:name]
+# Due to a Chef quirk we can't pass our params to another definition
+web_app 'phpapp' do
+  template 'site.conf.erb'
+  docroot docroot
+  server_name server_name
+end
+end
--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -31,60 +31,10 @@ mysql_database_user node['phpapp']['db_username'] do
   action :grant
 end
 
-# download and extract wordpress
-wordpress_latest = Chef::Config[:file_cache_path] + "/wordpress-latest.tar.gz"
-
-remote_file wordpress_latest do
-  source "http://wordpress.org/latest.tar.gz"
-  mode "0644"
-  checksum 'c419de49816b6483ab387567222898c02e8acd6ce64d6466a38f2fc05ebefb85'
-end
-
-directory node["phpapp"]["path"] do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
-  recursive true
-end
-
-execute "untar-wordpress" do
-  cwd node['phpapp']['path']
-  command "tar --strip-components 1 -xzf " + wordpress_latest
-  creates node['phpapp']['path'] + "/wp-settings.php"
-end
-
-#install wp
-wp_secrets = Chef::Config[:file_cache_path] + '/wp-secrets.php'
-
-remote_file wp_secrets do
-  source 'https://api.wordpress.org/secret-key/1.1/salt/'
-  action :create_if_missing
-  mode 0644
-end
-
-ruby_block 'fetch-salt-data' do
-  block do
-    salt_data = File.read(wp_secrets)
-  end
-  action :create
-end
-salt_data = File.read(wp_secrets)
-
-template node['phpapp']['path'] + '/wp-config.php' do
-  source 'wp-config.php.erb'
-  mode 0755
-  owner 'root'
-  group 'root'
-  variables(
-    :database        => node['phpapp']['database'],
-    :user            => node['phpapp']['db_username'],
-    :password        => node['phpapp']['db_password'],
-    :wp_secrets      => salt_data)
-end
-
-web_app 'phpapp' do
-  template 'site.conf.erb'
-  docroot node['phpapp']['path']
-  server_name node['phpapp']['server_name']
+wordpress_site node["phpapp"]["server_name"] do
+  path          "/var/www/phpapp"
+  database      node["phpapp"]["database"]
+  db_username   node["phpapp"]["db_username"]
+  db_password   node["phpapp"]["db_password"]
+  template      "site.conf.erb"
 end
